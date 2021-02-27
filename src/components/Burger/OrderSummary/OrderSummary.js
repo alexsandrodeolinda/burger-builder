20f7623f934bcb08cfc79eb944df471acfbcc219
Alexsandro <alexsandro.olinda@gmail.com>
2021-02-27 12:42:18 -0300

    Added toolbar

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -20,7 +20,10 @@ const orderSummary = (props) => {
 
             <ul>
                 {ingredientSummary}
-            </ul>     
+            </ul>  
+
+            <p><strong>{props.price.toFixed(2)}</strong></p>   
+
             <p>Continue to checkout?</p>
             <Button btnType="Danger"  clicked={props.purchaseCancelled}>CANCEL</Button>
             <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>             
