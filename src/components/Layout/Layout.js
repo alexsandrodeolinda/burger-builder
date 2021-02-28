d8c891f5237e8a36ca4f93c3571b4b205c6da9d5
Alexsandro <alexsandro.olinda@gmail.com>
2021-02-28 19:20:24 -0300

    Added menu and sidedrop

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,18 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Aux from '../../hoc/Aux'
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 import classes from './Layout.css';
 
-const layout = (props) => (
-    <Aux>
-       <Toolbar></Toolbar>
-       <SideDrawer></SideDrawer>
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Aux>
+class Layout extends Component {
+    state = {
+        showSideDrawer: false
+    }
     
-);
+    sideDrawerClosedHandler = () => {
+        this.setState({showSideDrawer: false});
+    }
 
-export default layout;
+    sideDrawerToggleHandler = () => {
+        this.setState( (prevState) => {
+            return {showSideDrawer: !prevState.showSideDrawer};
+        } );
+    }
+
+    render() {
+        return (
+            <Aux>
+                <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler}></Toolbar>
+                <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerClosedHandler}></SideDrawer>
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Aux>
+        )
+    }
+}
+export default Layout;
